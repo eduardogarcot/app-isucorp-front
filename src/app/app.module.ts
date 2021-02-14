2d4a080f3fe892a072d1ffd38ccf54f96b29259b
eduardogarcot <eduardogarcot@gmail.com>
2021-02-14 18:53:27 -0500

    adding contact item component and makin logic contact container

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { ContactItemComponent } from './contact-item/contact-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    ContactItemComponent
   ],
   imports: [
     BrowserModule,

0c158c820924b391e985b85b599aba459b3be036
eduardogarcot <eduardogarcot@gmail.com>
2021-02-14 20:07:52 -0500

    adding style to reservation item component 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {Contact} from './common/contact';
+import {Reservation} from './common/reservation';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +8,17 @@ import {Contact} from './common/contact';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  contact: Contact = {name: 'Felipe Garcia', phoneNumber: 18008976895, contactType: 'Contact Type 1', birthDate: new Date(1996, 11, 17)};
+  contact: Contact = {
+    name: 'Felipe Garcia',
+    phoneNumber: 18008976895,
+    contactType: 'Contact Type 1',
+    birthDate: new Date(1996, 11, 17)
+  };
+  reservation: Reservation = {
+    id: 1,
+    name: 'Noa La Goa',
+    reservationDate : new Date(2020, 12, 26, 18),
+    isfavorite: true,
+    rate: 5
+  };
 }

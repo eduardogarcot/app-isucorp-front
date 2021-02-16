28e23fba16ba7272a224a3f0ac5bc24013939ca2
eduardogarcot <eduardogarcot@gmail.com>
2021-02-16 12:14:44 -0500

    adding correcting reservation-list

--- src/app/reservation-list/reservation-list.component.ts
+++ src/app/reservation-list/reservation-list.component.ts
@@ -13,58 +13,69 @@ const urlPath = 'https://localhost:5001/api/reservations';
 })
 export class ReservationListComponent{
 
-  constructor(private http: HttpClient){
-    this.http.get(urlPath).subscribe(response => {
-      this.reservations = Object.keys(response).map(key => {
-        return this.getReservation(response[key]) as Reservation;
-      });
-      this.reservationsToShow = this.selectItemToShow(this.reservations, 1);
-    });
-  }
-
-  public reservations: object[];
-  public reservationsToShow: (Reservation) [];
+  public flag = false;
+  public reservations: Reservation[];
+  public reservationsToShow: Reservation[];
   @Input() numberOfItemsPerPage: number;
   public numberOfPages: number;
   form = new FormGroup({sortOrder: new FormControl('', )});
 
-  static sortItemsBy(items: any[], category: string, reverse: boolean = false): any[]{
+  static sortItemsBy(items: any[], category: string, reverse: boolean = false): Reservation[]{
     return reverse ? _.sortBy( items, category ).reverse() : _.sortBy( items, category );
   }
-  getReservation(item: object): Reservation{
-    return {
-        reservationId: item.reservationId,
-        contactName: 'LOGIC TO GET BY ID',
-        reservationDate : new Date((item.reservationDate as string).substring(0, 10)),
-        isFavorite: item.isFavorite,
-        rate: item.rate,
-        contactId: item.contactId,
-        cratedDate: new Date((item.cratedDate as string).substring(0, 10))
-      };
+
+  constructor(private http: HttpClient){
+    this.reservations = [] ;
+    this.http.get(urlPath).subscribe(response => {
+      let value = Object.keys(response).length;
+      Object.keys(response).map(key => {
+        const item = response[key];
+        const path = 'https://localhost:5001/api/contacts/' + item['contactId'];
+        this.http.get(path)
+          .subscribe(response => {
+            // tslint:disable-next-line:max-line-length
+            const reservation = {contactName: response['name'], reservationId: item.reservationId, reservationDate : new Date((item.reservationDate as string).substring(0, 10)),
+            isFavorite: item.isFavorite, rate: item.rate, contactId: item.contactId,
+            cratedDate: new Date((item.cratedDate as string).substring(0, 10))};
+            this.reservations.push(reservation as Reservation);
+            value--;
+            if (value === 0) {
+              console.log(this.reservations);
+              this.updateData();
+            }
+          });
+      });
+    });
   }
 
   isReverseSort(): boolean{
     return (this.form.get('sortOrder').value as string).indexOf(' ') >= 0;
   }
 
-  onDeleteReservation(data): void {
-    console.log(data);
-    // const newURL = urlPath + '/' + data.reservationId;
-    // this.http.delete(newURL).subscribe(() => {
-    // const index = this.reservations.indexOf(data);
-    // this.reservations.splice(index, 1);
-    // });
+  updateData(): void{
+    this.reservationsToShow = this.selectItemToShow(this.reservations, 1);
+    console.log(this.reservationsToShow);
   }
 
-  selectItemToShow(items: object [], pageNumber: number): any[] {
-    const newReservations: (any)[] = [];
+  onDeleteReservation(data): void {
+    const newURL = urlPath + '/' + data.reservationId;
+    this.http.delete(newURL).subscribe(() => {
+      const index = this.reservations.indexOf(data);
+      this.reservations.splice(index, 1);
+      this.updateData();
+    });
+  }
+
+  selectItemToShow(items: Reservation [], pageNumber: number): any[] {
+    const newReservations: Reservation[] = [];
+    console.log(items);
     for (let i = (pageNumber - 1) * this.numberOfItemsPerPage ; i < this.numberOfItemsPerPage ; i++)
     {
-      if (i === items.length) { break; }
-      newReservations.push(items[i]);
+      if (items[i] !== null ) {newReservations.push(items[i]); }
     }
-    //  DEFINIR CATEGORIA DE ORDENAMIENTO
-    return ReservationListComponent.sortItemsBy(newReservations, 'birthDate', this.isReverseSort());
+    const category = (this.form.get('sortOrder').value.toString().split(' ')[0]);
+    console.log(category);
+    return ReservationListComponent.sortItemsBy(newReservations, category !== ''  ? category : 'name' , this.isReverseSort());
   }
 }
 

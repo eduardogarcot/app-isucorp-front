c6c1fa5e166e92f53a9d7460e961773f7ed48308
eduardogarcot <eduardogarcot@gmail.com>
2021-02-15 23:23:39 -0500

    creatin POST done (OK)

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -2,14 +2,17 @@ import { Component, OnInit } from '@angular/core';
 import {FormGroup, FormControl, Validators} from '@angular/forms';
 import {InputItemsValidator} from './contactInputs.validator';
 import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-contact-form',
   templateUrl: './contact-form.component.html',
   styleUrls: ['./contact-form.component.css']
 })
+
 export class ContactFormComponent implements OnInit {
   public Editor = ClassicEditor;
+
   form = new FormGroup( {
     name: new FormControl('', [Validators.required]),
     contactType: new FormControl('', [Validators.required]),
@@ -19,16 +22,20 @@ export class ContactFormComponent implements OnInit {
   });
 
   onSubmit(): void {
-    console.log(this.form.getRawValue());
-    console.log(this.form);
-    console.log(this.form.valid);
-
-    // for (const item of this.form.controls){
-    //   for()
-    // }
+    const item = {
+      name: this.form.get('name').value,
+      contactType: this.form.get('contactType').value,
+      phoneNumber: this.form.get('phoneNumber').value,
+      birthDate: new Date((this.form.get('birthDate').value as string))
+    };
+    console.log(item);
+    this.http.post('https://localhost:5001/api/contacts', item)
+      .subscribe( response => {
+        console.log(response);
+      });
   }
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
 

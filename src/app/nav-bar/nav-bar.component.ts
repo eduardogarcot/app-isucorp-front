d8f265d25d7cf82952badeb1fee62cb732959493
eduardogarcot <eduardogarcot@gmail.com>
2021-02-21 04:39:18 -0500

    adding style to nav-bar

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -8,29 +8,10 @@ import {Router} from '@angular/router';
 })
 
 export class NavBarComponent implements OnInit {
-  public href = '';
-  public buttonLabel = '';
-  public routerLink: string ;
-  constructor(private router: Router) { }
+  public href = 'Reservation System';
+  constructor() { }
 
   ngOnInit(): void {
-    this.href = this.router.url;
-    if (this.href === '/reservation-form') {
-      this.href = 'Create Reservation';
-      this.buttonLabel = 'Reservations List';
-      this.routerLink = 'reservations-list';
-    } else if (this.href === '/') {
-      this.href = 'Reservations List';
-      this.buttonLabel = 'Create Reservation';
-      this.routerLink = '/reservation-form';
-    } else if (this.href === '/reservation-form') {
-      this.href = 'Create a Reservation';
-      this.buttonLabel = 'Create a Contact';
-      this.routerLink = '/contact-form';
-    } else if (this.href === '/contacts-list') {
-      this.href = 'Contacts List';
-      this.buttonLabel = 'Create a Reservation';
-      this.routerLink = '/contact-list';
-    }
+
   }
 }

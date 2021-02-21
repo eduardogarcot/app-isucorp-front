7b8a5c5e16d9974cf6a91ce9fa68fed1972fca71
eduardogarcot <eduardogarcot@gmail.com>
2021-02-21 04:07:40 -0500

    adding style to contact-items

--- src/app/contact-item/contact-item.component.ts
+++ src/app/contact-item/contact-item.component.ts
@@ -9,6 +9,7 @@ import {Contact} from '../common/contact';
 export class ContactItemComponent {
   @Output() deleteContact = new EventEmitter<Contact>();
   @Input() contact: Contact;
+  public contactType = { 0: 'Contact Type 1', 1: 'Contact Type 2', 2: 'Contact Type 3'};
 
   onDeleteContact(contact: Contact): void {
     this.deleteContact.emit(contact);

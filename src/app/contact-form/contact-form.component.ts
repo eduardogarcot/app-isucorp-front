820a8989047931283f9cf96c07e913f487fd2b6c
eduardogarcot <eduardogarcot@gmail.com>
2021-03-23 15:54:17 -0400

    refactoring contact-form

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -5,8 +5,6 @@ import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 import {ContactService} from '../services/contact.service';
 import {Router, ActivatedRoute} from '@angular/router';
 
-
-
 @Component({
   selector: 'app-contact-form',
   templateUrl: './contact-form.component.html',
@@ -21,7 +19,6 @@ export class ContactFormComponent implements OnInit {
   ) {
     this.router.routeReuseStrategy.shouldReuseRoute = () => false;
   }
-
   private parameter: number;
   public Editor = ClassicEditor;
   form = new FormGroup( {
@@ -33,58 +30,67 @@ export class ContactFormComponent implements OnInit {
   });
 
   onSubmit(): void {
-    const item = {
+    const item = this.GetContactFromForms();
+    if (this.parameter === 0)
+    {
+      this.service.postContact(item)
+        .subscribe(
+          () => this.RedirectTo('/contacts-list'),
+          error => this.HandleError(error));
+    }
+    else
+    {
+      this.service.updateContact(item, this.parameter )
+        .subscribe(
+          () => this.RedirectTo('/contacts-list'),
+          error => this.HandleError(error));
+    }
+  }
+  GetContactFromForms(): any{
+    return {
       name: this.form.get('name').value,
       contactType: Number(this.form.get('contactType').value),
       phoneNumber: Number(this.form.get('phoneNumber').value),
-      birthDate: this.form.get('birthDate').value
-    };
-    if (this.parameter === 0){
-      this.service.postContact(item)
+      birthDate: this.form.get('birthDate').value};
+  }
+  RedirectTo(url: string): void{
+    this.router.navigateByUrl(url);
+  }
+  HandleError(error): void{
+    if (error.status === 400)
+      { alert('The phone number already exist in the System. Please try with another number'); }
+    else
+      {alert('An unexpected error occurred.'); }
+  }
+  UpdateParameterWithGetRouteParam(): void{
+    this.parameter = this.route.snapshot.paramMap.get('contactId') === 'new' ? 0 : Number(this.route.snapshot.paramMap.get('contactId'));
+  }
+  MappingResponseToFormFields(response): void{
+    this.form.setValue(
+      {
+        name : response['name'],
+        contactType : response['contactType'],
+        phoneNumber : response['phoneNumber'],
+        birthDate : response['birthDate'].substring(0, 10)
+      });
+  }
+  ngOnInit(): void {
+    this.UpdateParameterWithGetRouteParam();
+    if (this.parameter !== 0)
+    {
+      this.service.getContact(this.parameter)
         .subscribe(
-          () => {
-            this.router.navigateByUrl('/contacts-list');
+          response => {
+            this.MappingResponseToFormFields(response);
           },
           error => {
-            if (error.status === 400) { alert('The phone number already exist in the System. Please try with another number'); }
-            else {alert('An unexpected error occurred.'); }
-          }
-        );
-    }
-    else {
-      this.service.updateContact(item, this.parameter )
-        .subscribe(
-          () => {
+            if (error.status === 404)
+              { alert('This contact doesn\'t exist in the System.'); }
+            else
+              {alert('An unexpected error occurred.'); }
             this.router.navigateByUrl('/contacts-list');
-          },
-          error2 => {
-            if (error2.status === 400) { alert('This Phone number already exist, please change it and try again.'); }
-            else {alert('An unexpected error occurred.'); }
-          }
-        );
+          });
     }
-  }
-
-  ngOnInit(): void {
-    this.parameter = this.route.snapshot.paramMap.get('contactId') === 'new' ? 0 : Number(this.route.snapshot.paramMap.get('contactId'));
-    if (this.parameter === 0) {return ; }
-    this.service.getContact(this.parameter)
-      .subscribe(
-        response => {
-          this.form.setValue(
-            {
-              name : response['name'],
-              contactType : response['contactType'],
-              phoneNumber : response['phoneNumber'],
-              birthDate : response['birthDate'].substring(0, 10)
-            });
-        },
-        error1 => {
-          if (error1.status === 404) { alert('This contact doesn\'t exist in the System.'); }
-          else {alert('An unexpected error occurred.'); }
-          this.router.navigateByUrl('/contacts-list');
-        }
-      );
   }
 
 }

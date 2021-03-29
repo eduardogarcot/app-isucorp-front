061e7c14315f23f7f636db14ae69b8476df2f933
eduardogarcot <eduardogarcot@gmail.com>
2021-03-29 03:23:36 -0400

    extracting apiURL constant to a separate file. [Only surce of truth]

--- src/app/services/contact.service.ts
+++ src/app/services/contact.service.ts
@@ -1,33 +1,34 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {apiUrl} from '../common/constants';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactService {
-  private url = 'https://localhost:5001/api/contacts';
+  private controllerUrl = apiUrl + 'contacts';
 
   constructor(private http: HttpClient) { }
 
   // tslint:disable-next-line:typedef
   getContacts() {
-    return this.http.get(this.url);
+    return this.http.get(this.controllerUrl);
   }
   // tslint:disable-next-line:typedef
   getContact(id: number ) {
-    return this.http.get(this.url + '/' + id );
+    return this.http.get(this.controllerUrl + '/' + id );
   }
   // tslint:disable-next-line:typedef
   postContact(contact) {
-    return this.http.post(this.url, contact);
+    return this.http.post(this.controllerUrl, contact);
   }
   // tslint:disable-next-line:typedef
   updateContact(contact, id: number) {
-    return this.http.put(this.url + '/' + id, contact);
+    return this.http.put(this.controllerUrl + '/' + id, contact);
   }
   // tslint:disable-next-line:typedef
   deleteContact(id: number)
   {
-    return this.http.delete( this.url + '/' + id);
+    return this.http.delete( this.controllerUrl + '/' + id);
   }
 }
--- src/app/services/reservation.service.ts
+++ src/app/services/reservation.service.ts
@@ -1,33 +1,34 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {apiUrl} from '../common/constants';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReservationService {
-  private url = 'https://localhost:5001/api/reservations';
+  private controllerUrl = apiUrl + 'reservations';
 
   constructor(private http: HttpClient) { }
 
   // tslint:disable-next-line:typedef
   getReservations() {
-    return this.http.get(this.url);
+    return this.http.get(this.controllerUrl);
   }
   // tslint:disable-next-line:typedef
   getReservation(id: number ) {
-    return this.http.get(this.url + '/' + id );
+    return this.http.get(this.controllerUrl + '/' + id );
   }
   // tslint:disable-next-line:typedef
   postReservation(reservation) {
-    return this.http.post(this.url, reservation);
+    return this.http.post(this.controllerUrl, reservation);
   }
   // tslint:disable-next-line:typedef
   updateReservation(reservation, id: number) {
-    return this.http.put(this.url + '/' + id, reservation);
+    return this.http.put(this.controllerUrl + '/' + id, reservation);
   }
   // tslint:disable-next-line:typedef
   deleteReservation(id: number)
   {
-    return this.http.delete( this.url + '/' + id);
+    return this.http.delete( this.controllerUrl + '/' + id);
   }
 }
